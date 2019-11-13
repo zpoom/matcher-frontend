20030439488a67b84d1ace3b34d39c1fe321061f
Nisaruj <nisaruj@hotmail.com>
2019-11-13 14:00:42 +0700

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
-import { LandingPage } from './pages';
+import { LandingPage, WsDetailPage } from './pages';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <div className="app">
-      <LandingPage />
-    </div >
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path='/workshop/:id' component={WsDetailPage} />
+          <Route path='/' component={LandingPage} />
+        </Switch>
+      </div >
+    </Router>
   );
 }
 
--- src/pages/WsDetailPage/index.tsx
+++ src/pages/WsDetailPage/index.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { WsCalendarCard } from '../../components';
 import { Row } from 'antd';
 
-export default () => {
+interface WsParam {
+    match?: any
+}
+
+export default ({ match }: WsParam) => {
     return (
         <Row>
-            <WsCalendarCard/>
+            <h1>Workshop ID {match.params.id}</h1>
+            <WsCalendarCard />
         </Row>
     );
 }
