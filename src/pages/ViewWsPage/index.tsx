f166bce6e4a622747f56921933d27470465bb7db
Nisaruj <nisaruj@hotmail.com>
2019-11-23 11:07:57 +0700

    Add search workshop

--- src/pages/ViewWsPage/index.tsx
+++ src/pages/ViewWsPage/index.tsx
@@ -3,9 +3,11 @@ import { Row, Col } from 'antd'
 import { WorkshopCard, NavBarWithLogo } from '../../components'
 import './index.css'
 import { getAllWorkshops } from '../../api/workshop'
+import TextField from '@material-ui/core/TextField'
 
 export default () => {
   const [workshops, setWorkshops] = useState<any>([])
+  const [filter, setFilter] = useState('')
 
   const fetchWorkshops = () => {
     getAllWorkshops().then(workshops => setWorkshops(workshops))
@@ -17,16 +19,30 @@ export default () => {
     <NavBarWithLogo />
     <Row type="flex" justify="space-around" className="container">
       <Col xs={24}>
-        <h1>Manage Workshop</h1>
+        <h1>Workshops</h1>
+        <Row type="flex">
+          <Col xs={12}>
+            <TextField
+              label="Search"
+              variant="outlined"
+              placeholder="Search..."
+              fullWidth
+              onChange={e => setFilter(e.target.value)}
+            />
+          </Col>
+        </Row>
         <Row type="flex" justify="space-around" style={{ marginBottom: 30 }}>
           {
-            workshops.map((workshop: any) => <Col style={{ marginTop: 47 }}>
-              <WorkshopCard 
-                id={workshop.id} 
-                title={workshop.name} 
-                ownerName={workshop.owner_name}
-              />
-            </Col>)
+            workshops
+              .filter((ws: any) => ws.name.includes(filter))
+              .map((workshop: any) =>
+                <Col style={{ marginTop: 47 }}>
+                  <WorkshopCard
+                    id={workshop.id}
+                    title={workshop.name}
+                    ownerName={workshop.owner_name}
+                  />
+                </Col>)
           }
         </Row>
       </Col>
